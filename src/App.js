bba6f775748ed7cd11f63496f220c4aeb547f41e
elgiz0210 <dastanadilbekov2019@gmail.com>
2023-11-14 12:02:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage/HomePage';
+import CurrentPage from './pages/Current/CurrentPage'
+import WeatherPage from './pages/Weather/WeatherPage';
+
 
 function App() {
-  return (
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Routes>
+<Route exact path='/' element={<HomePage/> }/>
+<Route exact path='/Weather' element={<WeatherPage/> }/>
+<Route exact path='/Current' element={<CurrentPage/> }/>
+</Routes>
+
     </div>
   );
 }
